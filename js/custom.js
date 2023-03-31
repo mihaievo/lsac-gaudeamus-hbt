d2e2655fd617f264b59c1c57031a258f6a7f06e8
Mihai <53655845+mihaievo@users.noreply.github.com>
2023-03-31 22:31:29 +0300

    initial commit - mihaievo

---
+++ js/custom.js
@@ -0,0 +1,33 @@
+// FUNCTIE PT NAVBAR MOBILE
+  (function ($) {
+  
+  "use strict";
+
+    // MENIU
+    $('.navbar-collapse a').on('click',function(){
+      $(".navbar-collapse").collapse('hide');
+    });
+    
+    // lINKURI
+    $('.smoothscroll').click(function(){
+      var el = $(this).attr('href');
+      var elWrapped = $(el);
+      var header_height = $('.navbar').height();
+  
+      scrollToDiv(elWrapped,header_height);
+      return false;
+  
+      function scrollToDiv(element,navheight){
+        var offset = element.offset();
+        var offsetTop = offset.top;
+        var totalScroll = offsetTop-navheight;
+  
+        $('body,html').animate({
+        scrollTop: totalScroll
+        }, 300);
+      }
+    });
+  
+  })(window.jQuery);
+
+
